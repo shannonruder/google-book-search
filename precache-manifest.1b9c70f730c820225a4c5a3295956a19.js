283df17e1e370f4fd75c2cf249e71cf6cc1ca870
undacat <shannon@ruder.net>
2019-05-04 20:54:04 -0700

    Updates

---
+++ precache-manifest.1b9c70f730c820225a4c5a3295956a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2309f4a7192b8bc52f78",
+    "url": "/google-book-search/static/css/main.b6791f72.chunk.css"
+  },
+  {
+    "revision": "2309f4a7192b8bc52f78",
+    "url": "/google-book-search/static/js/main.2309f4a7.chunk.js"
+  },
+  {
+    "revision": "a7b15f834b0897cbf47c",
+    "url": "/google-book-search/static/js/1.a7b15f83.chunk.js"
+  },
+  {
+    "revision": "d766b99506104cb9494c",
+    "url": "/google-book-search/static/js/runtime~main.d766b995.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/google-book-search/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "d2b6501821da72454218fa9ef879b912",
+    "url": "/google-book-search/index.html"
+  }
+];
